41842768086014e205599dc3aa204568815b1981
Garrett Estrin <garrett.estrin@gmail.com>
2018-11-21 16:57:32 -0800

    About Desktop complete. Also added all assets for site

--- parts/about.php
+++ parts/about.php
@@ -1,3 +1,13 @@
-<section class="section section--white about mainview" id="jsAbout" vs-anchor="jsAbout">
-  This is the about section
+<section class="section section--white about mainview about" id="jsAbout" vs-anchor="jsAbout">
+  <div class="container">
+    <div class="about__content--cont">
+      <p class="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem veniam impedit nam repudiandae repellendus quam, dolor, ad eaque dicta assumenda hic tenetur, eius necessitatibus perferendis doloribus veritatis id quas? Incidunt.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo repellat, rem ad asperiores recusandae doloribus quaerat atque quidem optio sequi velit, sunt, dolorum explicabo. Quam praesentium repudiandae eveniet maxime voluptatibus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae laudantium atque earum tenetur minima culpa modi, iure impedit nemo maxime ut nobis sapiente omnis fugit perspiciatis quidem optio voluptate amet?
+      </p>
+    </div>
+    <div class="about__content--cont">
+      <img src="/dist/assets/img/2_About/about_photo.jpeg" alt="">
+    </div>
+  </div>
 </section>
