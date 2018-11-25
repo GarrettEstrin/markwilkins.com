088cd21cdddaa92adcc812f55b53b3c433902fa7
Garrett Estrin <garrett.estrin@gmail.com>
2018-11-25 15:03:56 -0800

    fixed friends logos on mobile devices

--- parts/friends.php
+++ parts/friends.php
@@ -1,21 +1,6 @@
 <section class="section section--black mainview friends" id="jsFriends" vs-anchor="jsFriends">
   <div class="container">
     <div class="friends__title">Some friends I've worked with.</div>
-    <!-- <div class="friends__flex">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_barbie.png" alt="" class="friends__img">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_hotwheels.png" alt="" class="friends__img">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_disney.png" alt="" class="friends__img">
-    </div>
-    <div class="friends__flex">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_nickelodeon.png" alt="" class="friends__img">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_dreamworks.png" alt="" class="friends__img">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_sony.png" alt="" class="friends__img">
-    </div>
-    <div class="friends__flex">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_americangirl.png" alt="" class="friends__img">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_foxconn.png" alt="" class="friends__img">
-      <img src="/dist/assets/img/5_Friends/friends_thumb_acer.png" alt="" class="friends__img">
-    </div> -->
     <div class="friends__flex">
     <img src="/dist/assets/img/5_Friends/friends_thumb_barbie.png" alt="" class="friends__img">
       <img src="/dist/assets/img/5_Friends/friends_thumb_hotwheels.png" alt="" class="friends__img">
