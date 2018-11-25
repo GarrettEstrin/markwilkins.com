7d13b4bf254b38aaa09b9875c3b5e24aa36c1549
Garrett Estrin <garrett.estrin@gmail.com>
2018-11-25 10:38:31 -0800

    Added video playing ability on projects page.  Need to get last videos

--- parts/projects.php
+++ parts/projects.php
@@ -1,3 +1,29 @@
-<section class="section section--black mainview" id="jsProjects" vs-anchor="jsProjects">
-  This is the projects section
+<section class="section section--black mainview projects" id="jsProjects" vs-anchor="jsProjects">
+  <div class="container">
+    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157377444" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+      <div class="projects__play"></div>
+    </div>
+    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/191757408" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+      <div class="projects__play"></div>
+    </div>
+    <div class="projects__cont">
+      <div class="projects__play"></div>
+    </div>
+    <div class="projects__cont project__cont--mobile">
+      <div class="projects__play"></div>
+    </div>
+    <div class="projects__cont project__cont--mobile">
+      <div class="projects__play"></div>
+    </div>
+    <div class="projects__cont project__cont--mobile">
+      <div class="projects__play"></div>
+    </div>
+  </div>
+  <script>
+    function playVideo(el){
+      let videoLinkEl = el.dataset.video;
+      $(el).html(videoLinkEl);
+      el.classList.add('projects__cont--noBG');
+    }
+  </script>
 </section>
