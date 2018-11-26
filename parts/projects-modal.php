5d9a70eec9272157bdeec1a63244419d32606a73
Garrett Estrin <garrett.estrin@gmail.com>
2018-11-26 11:54:35 -0800

    video modal added

---
+++ parts/projects-modal.php
@@ -0,0 +1,10 @@
+<section class="projects__modal modal hidden" id="jsModal" onClick="closeVideo()">
+  <div class="container">
+    <div class="modal__video-cont" id="jsVideoCont"></div>
+  </div>
+  <script>
+    function closeVideo(){
+      $('#jsModal').addClass('hidden');
+    }
+  </script>
+</section>
--- parts/projects.php
+++ parts/projects.php
@@ -6,24 +6,25 @@
     <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/191757408" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont">
+    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157370850" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont project__cont--mobile">
+    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/146453353" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont project__cont--mobile">
+    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/191757408" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont project__cont--mobile">
+    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/191757408" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
   </div>
   <script>
     function playVideo(el){
       let videoLinkEl = el.dataset.video;
-      $(el).html(videoLinkEl);
-      el.classList.add('projects__cont--noBG');
+      $('#jsModal').removeClass('hidden');
+      $('#jsVideoCont').html(videoLinkEl);
     }
   </script>
+  <?php include 'projects-modal.php'; ?>
 </section>
