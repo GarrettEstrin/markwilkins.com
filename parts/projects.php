0b2e0d92b57a35f65145d42f78b5e761f1ec6644
Garrett Estrin <garrett.estrin@gmail.com>
2018-12-28 19:07:45 -0800

    adjusted video container

--- parts/projects.php
+++ parts/projects.php
@@ -1,21 +1,21 @@
 <section class="section section--black mainview projects" id="jsProjects" vs-anchor="jsProjects">
   <div class="container">
-    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157377444" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157377444" width="100%" height="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/191757408" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/191757408" width="100%" height="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157377443" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+    <div class="projects__cont" onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157377443" width="100%" height="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/146453353" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/146453353" width="100%" height="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/189711531" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/189711531" width="100%" height="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
-    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157370850" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
+    <div class="projects__cont project__cont--mobile"  onClick="playVideo(this)" data-video='<iframe src="https://player.vimeo.com/video/157370850" width="100%" height="95%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'>
       <div class="projects__play"></div>
     </div>
   </div>
